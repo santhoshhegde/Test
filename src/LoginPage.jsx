c131a645cf37e34b91dd1089ca8c2cca25139fcc
Santhosh <santhosh.hegde262000@gmail.com>
2024-07-26 17:42:23 +0530

    Initial commit

---
+++ src/LoginPage.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useState();
+  const [isLoginAttempt, setIsLoginAttempt] = useState(false);
+  const authentication = () => {
+    if (login === "santhosh" && password === "12345") {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+    setIsLoginAttempt(true);
+  };
+  return (
+    <div className="bg-[#e6e8e6] h-screen w-screen flex items-center">
+      <div className="flex flex-col gap-[2.5vh] w-[30vw]  mx-auto bg-white p-[5vw] bottom-4 rounded-lg ">
+        <h1 className="text-3xl font-bold">Login</h1>
+        <label htmlFor="loginInput">Login</label>
+        <input
+          className="bg-[#e6e8e6] border-none p-3 rounded"
+          type="text"
+          id="loginInput"
+          placeholder="Email"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
+        <label htmlFor="passwordInput">Password</label>
+        <input
+          className="bg-[#e6e8e6] border-none p-3 rounded"
+          type="password"
+          id="passwordInput"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="bg-blue-700 text-white p-3 border-none rounded-lg"
+          onClick={authentication}
+        >
+          Login
+        </button>
+        {isLoginAttempt ? auth ? <h1>Sucessful</h1> : <h1>Fail</h1> : <h1></h1>}
+        <hr className="w-full opacity-50" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/LoginSucess.jsx
@@ -0,0 +1,11 @@
+// import { Link } from "react-router-dom";
+
+// const LoginSucesssful = () => {
+//   return (
+//     <h1>
+//       <Link to="/">Login Sucessfull</Link>
+//     </h1>
+//   );
+// };
+
+// export default LoginSucesssful;
