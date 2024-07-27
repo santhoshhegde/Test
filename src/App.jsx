4418c3c799e41987674c309f3e89d2c9f8241268
Santhosh <santhosh.hegde262000@gmail.com>
2024-07-27 16:40:54 +0530

    Lenovo:"dashboard, login, logout, routing"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import "./App.css";
-import EmployeeForm from "./EmployeeForm";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "./LoginPage";
+import Dashboard from "./Dashboard";
+import EmployeeList from "./EmployeeList";
+import EmployeeForm from "./EmployeeForm";
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      <EmployeeForm />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+          <Route path="/create-employee" element={<EmployeeForm />} />
+          <Route path="/employee-list" element={<EmployeeList />} />
+        </Routes>
+      </Router>
     </>
   );
 }
